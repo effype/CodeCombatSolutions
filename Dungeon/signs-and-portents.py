68cf38c4e9ec0cefb2fb2e10c22244ab01b4ad2f
effype <57405001+effype@users.noreply.github.com>
2020-01-25 11:46:12 +0100

    Create signs-and-portents.py

---
+++ Dungeon/signs-and-portents.py
@@ -0,0 +1,14 @@
+# Twoim celem jest dostanie się do prawej strony mapy żywym.
+# Nie musisz walczyć z ogrami, tylko ruszaj się! Twoi sojusznicy będą cię ochraniać.
+hero.moveRight()
+hero.moveRight()
+hero.moveUp()
+hero.moveRight()
+hero.moveRight()
+hero.moveRight()
+hero.moveDown()
+hero.moveRight()
+hero.moveDown()
+hero.moveRight()
+hero.moveRight()
+hero.moveRight()
