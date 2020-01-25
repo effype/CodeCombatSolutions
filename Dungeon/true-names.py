3bc0fa7fbe8070b400b8327958c12abc11c589e0
effype <57405001+effype@users.noreply.github.com>
2020-01-25 11:48:20 +0100

    Create true-names.py

---
+++ Dungeon/true-names.py
@@ -0,0 +1,11 @@
+# Broń się przeciwko Brak i Treg!
+# Musisz dwukrotnie zaatakować małe ogry.
+
+hero.moveRight()
+hero.attack("Brak")
+hero.attack("Brak")
+hero.moveRight()
+hero.attack("Treg")
+hero.attack("Treg")
+hero.moveRight()
+hero.moveRight()
